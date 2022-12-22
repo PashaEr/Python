f1122746588a1ada41d7b93dda1abdeda8c801b1
Pavel Ershov <pasha_MV2011@mail.ru>
2022-12-22 10:20:23 +0300

    ДЗ второй семинар_

--- Seminar_2_hw/hw2.py
+++ Seminar_2_hw/hw2.py
@@ -44,17 +44,19 @@
 
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-n = int(input('Введите число N: '))
-my_list = []
-sum_elements = 0
+# n = int(input('Введите число N: '))
+# my_list = []
+# sum_elements = 0
 
 
-for item in range(1, n + 1):
-    elem = round((item + (1 / item)) ** item,2)
-    my_list.append(elem)
-    sum_elements += elem
-print(my_list)
-print(sum_elements)
+# for item in range(1, n + 1):
+#     elem = round((item + (1 / item)) ** item,2)
+#     my_list.append(elem)
+#     sum_elements += elem
+# print(my_list)
+# print(sum_elements)
+
+
 
 
 
